78e0be46a895e6b6a9a4569fd9fac7dbe8413f33
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-11-25 23:26:08 +0530

    footer section done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import Advertisement from "../../components/Advertisement/Advertisement";
 import Banner from "../../components/Banner/Banner";
 import MyContainer from "../../components/Shared/MyContainer";
+import Footer from "../../components/Footer/Footer";
 
 const Home = () => {
   return (
     <MyContainer>
       <Banner />
       <Advertisement />
+      <Footer />
     </MyContainer>
   );
 };
